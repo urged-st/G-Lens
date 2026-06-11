cf42290f96c0ae8dee8b095d9356e4c94c944198
Arjun <1forarjun@gmail.com>
2026-06-11 22:44:48 +0100

    shader prep, save initial index as prototype

---
+++ sketch.js
@@ -0,0 +1,23 @@
+let theShader;
+
+function preload() {
+  theShader = loadShader('shader.vert', 'shader.frag');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  noStroke();
+}
+
+function draw() {
+  shader(theShader);
+
+  theShader.setUniform('u_resolution', [width, height]);
+  theShader.setUniform('u_time', millis() / 1000.0);
+
+  rect(0, 0, width, height);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
